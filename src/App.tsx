6bfa48b69c17341ba68079a60e0a20312e7dfd47
chrjl <73675826+chrjl@users.noreply.github.com>
2024-11-24 21:02:06 -0800

    fix(navigation): navigate from root route

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-function App() {
-  return <h1>Hello World!</h1>;
-}
+import { Navigate } from 'react-router';
 
-export default App;
+export default function App() {
+  return <Navigate to="/itineraries" />;
+}
