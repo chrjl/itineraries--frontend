7d91e3e10d0cfb5d54a5812f8f83d8f445057dcb
chrjl <73675826+chrjl@users.noreply.github.com>
2024-11-26 09:55:53 -0800

    refactor: delete unused routes

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,6 @@ import './mvp.css';
 import Itineraries from './components/Itineraries.tsx';
 import Itinerary from './components/Itinerary.tsx';
 import CreateItinerary from './components/CreateItinerary.tsx';
-import CreateActivity from './components/CreateActivity.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -16,7 +15,6 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/" element={<Itineraries />} />
         <Route path="/new" element={<CreateItinerary />} />
         <Route path="/:id" element={<Itinerary />} />
-        <Route path="/:id/create" element={<CreateActivity />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
